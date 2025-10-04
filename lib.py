f83eade2e57e6b256b3e105748799c92366a8f95
Andres Castro <acastro3@my.westga.edu>
2025-10-04 15:30:54 -0400

    Create required skeleton

---
+++ lib.py
@@ -0,0 +1,10 @@
+def encode_to_base64(txt: str) -> bytes:
+    """Accept a string, encode it in Base64, and return the encoded string.
+
+    Args:
+        txt (str): String to encode
+
+    Returns:
+        bytes: The encoded string
+    """
+    return b""
---
+++ main.py
@@ -0,0 +1,22 @@
+def start_data_transfer() -> None:
+    """Client algorithm
+    1. read input
+    1. start socket connection
+    1. print connected
+    1. send branch code
+    1. log ok from server
+    1. send encoded file
+    1. print received
+    1. log ok from server
+    """
+
+    pass
+
+
+def main() -> None:
+    """Entry point into the program."""
+    start_data_transfer()
+
+
+if __name__ == "__main__":
+    main()
